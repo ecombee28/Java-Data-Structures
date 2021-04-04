ce40c4024942bc6cf40cda86bb5b5cdaaabdc233
Eric Combee <45884566+ecombee28@users.noreply.github.com>
2021-04-03 23:16:25 -0700

    Added Binary Search

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,7 @@ public class Main{
         int[] arr3 = generateRandomArray(5000);
         int[] arr4 = generateRandomArray(5000);
         int[] arr5 = generateRandomArray(5000);
+        int[] arr6 = {2,3,3,4,5,6,7,8,9,10,15,11,20,34,40,44};
         int start, stop;
 
 
@@ -21,6 +22,7 @@ public class Main{
         ShellSort shellSort  = new ShellSort();
         MergeSort mergeSort = new MergeSort();
         QuickSort quickSort = new QuickSort();
+        BinarySearch binarySearch = new BinarySearch();
 
         start = (int)ZonedDateTime.now().toInstant().toEpochMilli();
         System.out.print("Bubble sort before:  ");
@@ -73,6 +75,24 @@ public class Main{
         quickSort.doQuickSort(arr5);
         stop = (int)ZonedDateTime.now().toInstant().toEpochMilli();
         System.out.println("Duration: "+(stop - start)+" milliseconds");
+        
+        start = (int)ZonedDateTime.now().toInstant().toEpochMilli();
+        bubbleSort(arr6);
+        System.out.print("\nBinary Search before:  ");
+        System.out.println(Arrays.toString(arr6));
+        
+        int n = arr6.length;
+        int findIndex = 11;
+        int result = BinarySearch.binarySearch(arr6, 0, n - 1, findIndex);
+        if (result == -1) {
+            System.out.println("Element not present");
+        }else {
+            System.out.println("Element found at index " + result);
+        }
+        
+        
+        stop = (int)ZonedDateTime.now().toInstant().toEpochMilli();
+        System.out.println("Duration: "+(stop - start)+" milliseconds");
 
     }
 
@@ -80,10 +100,29 @@ public class Main{
         int[] arr = new int[size];
 
         for(int i=0;i< size;i++){
-            int random = (int) (Math.random()*700) + 1;
+            int random = (int) (Math.random()*10) + 1;
             arr[i] = random;
         }
 
         return arr;
+    }
+    
+    public static int[] bubbleSort(int[] arr){
+        int temp;
+        int n  = arr.length;
+
+        for(int i=0;i<n;i++){
+            for(int j=1;j<n-i;j++){
+                if(arr[j-1] > arr[j]){
+                    temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+
+            }
+        }
+       
+		return arr;
+
     }
 }
